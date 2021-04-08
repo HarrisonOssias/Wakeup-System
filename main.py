d27db41677375ad9a526fc71645e6ace3e3e22a0
HarrisonOssias <ossiash@mcmaster.ca>
2021-04-08 02:25:26 -0400

    Testing tweet thread

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import time
 import threading 
-import RPi.GPIO as GPIO
-from actions.alarm import Buzzer
-from actions.slapper import Stepper
+#import RPi.GPIO as GPIO
+#from actions.alarm import Buzzer
+#from actions.slapper import Stepper
 from routes.tweets import BlinkyStreamer
 from dotenv import dotenv_values #must be using .env file to store twitter API keys
 
 config = dotenv_values(".env") #get keys from .env file and store in dictionary  
 
 # Search terms
-TERMS = '@HDO08658143'
+TERMS = '#HOssias'
 
 # Setup GPIO as output
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, GPIO.LOW)
-
-
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(LED, GPIO.OUT)
+#GPIO.output(LED, GPIO.LOW)
 
 
 # Create streamer
--- routes/tweets.py
+++ routes/tweets.py
@@ -1,10 +1,53 @@
 from twython import TwythonStreamer
+from collections import deque   
+from requests.exceptions import ChunkedEncodingError
 
 # Setup callbacks from Twython Streamer
 class BlinkyStreamer(TwythonStreamer):
+    def __init__(self, consumer_key, consumer_secret, token, token_secret, tqueue):
+        self.tweet_queue = tqueue
+        super(TwitterStream, self).__init__(consumer_key, consumer_secret, token, token_secret)
+
     def on_success(self, data):
-        n = 0
-        while (n <= 10):
-            if 'text' in data:
-                print (data['text'] + "Was added to queue") #set tweet
-                n++
+        if 'text' in data:
+            self.tweet_queue.append(data)
+
+    def on_error(self, status_code, data):
+        print(status_code)
+        # Want to stop trying to get data because of the error?
+        # Uncomment the next line!
+        # self.disconnect()
+
+ TERMS = "HOSSIAS"               
+                
+config = dotenv_values(".env") #get keys from .env file and store in dictionary  
+
+def stream_tweets(tweets_queue):
+
+    try:
+        stream = BlinkyStreamer(config["APP_KEY"], config["APP_SECRET"], config["OAUTH_TOKEN"], config["OAUTH_TOKEN_SECRET"])
+
+        # You can filter on keywords, or simply draw from the sample stream
+        #stream.statuses.filter(track='twitter', language='en')
+        stream.statuses.sample(track=TERMS)
+    except ChunkedEncodingError:
+        # Sometimes the API sends back one byte less than expected which results in an exception in the
+        # current version of the requests library
+        stream_tweets(tweet_queue)
+
+        
+            
+def process_tweets(tweets_queue):
+    while True:
+        if len(tweets_queue) > 0:
+            #  Do something with the tweets
+            print(tweets_queue.popleft())
+
+if __name__ == '__main__':
+
+    tweet_queue = deque()
+
+    tweet_stream = Thread(target=stream_tweets, args=(tweet_queue,), daemon=True)
+    tweet_stream.start()
+
+    process_tweets(tweet_queue)
