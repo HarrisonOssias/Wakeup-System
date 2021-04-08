0e675e9ad1d580e77436f0bfb32b55e019803671
HarrisonOssias <ossiash@mcmaster.ca>
2021-04-08 16:53:42 -0400

    'changed to BCM'

--- actions/water.py
+++ actions/water.py
@@ -1,25 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 class Pump:
     def __init__(self):
-        self.pumpPin = 40 # GPIO 16
+        self.pumpPin = 21 # GPIO 16
         self.pumpOn = False
         GPIO.setup(self.pumpPin, GPIO.OUT)
     def switchPump(self):
-        if (self.pumpOn == True):
-            self.pumpOn = False
-        else:
-            self.pumpOn = True
         GPIO.output(self.pumpPin, GPIO.HIGH)
+        self.pumpOn = True
+        print (self.pumpOn)
         time.sleep(3)
         GPIO.output(self.pumpPin, GPIO.LOW)
+        self.pumpOn = False
+        print (self.pumpOn)
+        time.sleep(3)
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
     pump = Pump()
-
     pump.switchPump()
-    time.sleep(3)
- 
